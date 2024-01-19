77ce0a79907b209e976daebdbb0e804253c4e1ce
Dylan Welch <dylan.welch@delta.com>
2024-01-19 12:11:53 -0500

    Initial Commit

---
+++ Atlanta Weather/Atlanta_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Atlanta_WeatherApp.swift
+//  Atlanta Weather
+//
+//  Created by Dylan Welch on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Atlanta_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Atlanta WeatherUITests/Atlanta_WeatherUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Atlanta_WeatherUITestsLaunchTests.swift
+//  Atlanta WeatherUITests
+//
+//  Created by Dylan Welch on 1/19/24.
+//
+
+import XCTest
+
+final class Atlanta_WeatherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
